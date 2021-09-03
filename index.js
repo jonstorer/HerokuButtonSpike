a812a156e66f85db46458ca0c3c0dd6c14180a3a
Jonathon Storer <jstorer@salesforce.com>
2021-09-03 11:30:36 -0400

    wip

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const PORT = process.env.PORT || 3030;
 
 app.use(morgan('dev'));
 app.get('*', function (req, res, next) {
   res.json(process.env);
 });
 
-app.listen(process.env.PORT, function (err) {
-  console.log('listneing on port', process.env.PORT);
+app.listen(PORT, function (err) {
+  console.log('listneing on port', PORT);
 })
